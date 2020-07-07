fcc70e3a13fed8375fe0357a04ace19c2b144198
Jared Lunde <jared.lunde@gmail.com>
2020-07-06 20:49:23 -0600

    feat: create useKey hook

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,9 +1,72 @@
-// import React from 'react'
-// import {renderHook} from '@testing-library/react-hooks'
-// import {render} from '@testing-library/react'
-// import userEvent from '@testing-library/user-event
-describe('use-key', () => {
-  it('should pass', () => {
-    expect(true).toBe(true)
+import * as React from 'react'
+import {render, fireEvent, screen} from '@testing-library/react'
+import useKey from './index'
+
+describe('useKey()', () => {
+  it('should call function when keycode matches', () => {
+    const mock = jest.fn()
+
+    const Component = () => {
+      const ref = React.useRef(null)
+      useKey(ref, {Escape: mock})
+      return <button data-testid='btn' ref={ref} />
+    }
+
+    render(<Component />)
+    fireEvent.keyDown(screen.getByTestId('btn'), {key: 'Escape'})
+    expect(mock).toBeCalled()
+  })
+
+  it('should not call function when keycode does not match', () => {
+    const mock = jest.fn()
+
+    const Component = () => {
+      const ref = React.useRef(null)
+      useKey(ref, {Escape: mock})
+      return <button data-testid='btn' ref={ref} />
+    }
+
+    render(<Component />)
+    fireEvent.keyDown(screen.getByTestId('btn'), {key: 'Enter'})
+    expect(mock).not.toBeCalled()
+  })
+
+  it('should listen to window events', () => {
+    const mock = jest.fn()
+
+    const Component = () => {
+      useKey(window, {Escape: mock})
+      return <button />
+    }
+
+    render(<Component />)
+    fireEvent.keyDown(window, {key: 'Escape'})
+    expect(mock).toBeCalled()
+  })
+
+  it('should listen to document events', () => {
+    const mock = jest.fn()
+
+    const Component = () => {
+      useKey(document, {Escape: mock})
+      return <button />
+    }
+
+    render(<Component />)
+    fireEvent.keyDown(document, {key: 'Escape'})
+    expect(mock).toBeCalled()
+  })
+
+  it('should listen to body events', () => {
+    const mock = jest.fn()
+
+    const Component = () => {
+      useKey(document.body, {Escape: mock})
+      return <button />
+    }
+
+    render(<Component />)
+    fireEvent.keyDown(document.body, {key: 'Escape'})
+    expect(mock).toBeCalled()
   })
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,36 @@
-// import React from 'react'
-const Component = (): string => 'Hello world'
-export default Component
+import * as React from 'react'
+import useEvent from '@react-hook/event'
+
+export function useKey<T extends Window>(
+  target: T | null,
+  listeners: Record<string, (event: KeyboardEvent) => any>
+): void
+export function useKey<T extends Document>(
+  target: T | null,
+  listeners: Record<string, (event: KeyboardEvent) => any>
+): void
+export function useKey<T extends HTMLElement>(
+  target: React.RefObject<T> | T | null,
+  listeners: Record<string, (event: KeyboardEvent) => any>
+): void
+export function useKey(
+  target: any,
+  listeners: Record<string, (event: KeyboardEvent) => any>
+) {
+  useEvent(target, 'keydown', (event): void => {
+    const listener = listeners[IE_COMPAT[event.key] || event.key]
+    if (listener) listener(event)
+  })
+}
+
+// IE 11 and some versions of Edge have non-standard value
+const arrow = 'Arrow'
+const IE_COMPAT: Record<string, string> = {
+  Up: arrow + 'Up',
+  Right: arrow + 'Right',
+  Down: arrow + 'Down',
+  Left: arrow + 'Left',
+  Esc: 'Escape',
+}
+
+export default useKey
